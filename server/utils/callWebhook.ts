730bfda1e825d3a112a496990a2397b24f6f344d
Alex Riviere <alex.riviere+github@nexcortech.com>
2025-03-04 14:21:42 -0800

    just kidding!

--- server/utils/callWebhook.ts
+++ server/utils/callWebhook.ts
@@ -1,7 +1,7 @@
 import {createSignature} from "~~/server/utils/validationSignature";
 
 export async function callWebhook<T extends WebhookRequest>(webhook: T, payload: any) {
-  const datetime = process.env.NODE_ENV ==="development" ? "test" : Date.now();
+  const datetime = Date.now();
   const signedHeader = `sha256=${await createSignature(webhook.secret, datetime+JSON.stringify(payload))}`;
   return $fetch(webhook.callback,{
     method:"POST",
